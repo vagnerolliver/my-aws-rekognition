c7b635db16ec84d180364216ed2f6c35c9ddcc76
Vágner Oliveira <vagner.oliveira@zenvia.com>
2020-11-01 22:35:09 -0300

    feat: hello sls framework

---
+++ sls-framework/run.sh
@@ -0,0 +1,27 @@
+# instalar
+npm i -g serverless
+
+
+# sls incializar
+sls
+
+
+# sempre fazer o deploy do ambiente assim que você criou o projeto
+sls deploy
+
+
+#invocar
+sls invoke -f hello
+
+
+#invocar local
+sls invoke local -f hello
+
+
+# configurar dashboard
+sls
+
+
+# logs
+sls logs -f hello -t
+
